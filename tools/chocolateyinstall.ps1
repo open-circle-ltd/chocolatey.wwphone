5945f70ffc5ac4b8e26a4743bd3eb650ed093a04
Benaiah Catheraso <benaiah@itigo.ch>
2021-12-02 11:30:38 +0100

    Initial Commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://wwcom.ch/downloads/wwphone_3_4_9.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'wwphone*'
+  checksum      = 'FEBF205701DCE1B48791881CE1430D54A6A8EB4006A700B2FF54B48C28E76C58'
+  checksumType  = 'sha256'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs 
