3ea2992eea82aa9b309c71030dbf8cea41959fcb
kve-occ <210706414+kve-occ@users.noreply.github.com>
2025-11-28 11:30:47 +0100

    Feature: Include 32bit Parameter, include new parameters

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -1,17 +1,49 @@
 ﻿$ErrorActionPreference = 'Stop';
+$PackageParameters = Get-PackageParameters
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$urlPackage = 'https://wwcom.ch/downloads/wwphone_4_1_13.exe'
-$checksumPackage = '0127fffe4885bb24ed00b8c0deb6d52c5bb5fb9ec58bea6612430ca5525ec015a14d686ddd5d2eadfd52597f4368f533177d5376bcfe69bf74353c5e58b287b1'
+$url = 'https://wwcom.ch/downloads/wwphone_4_1_13.exe'
+$url64 = 'https://wwcom.ch/downloads/wwphone64_4_1_13.exe'
+$checksum = '0127fffe4885bb24ed00b8c0deb6d52c5bb5fb9ec58bea6612430ca5525ec015a14d686ddd5d2eadfd52597f4368f533177d5376bcfe69bf74353c5e58b287b1'
+$checksum64 = '955bd4620eb1600fd29bae4d4cd1da84743306b3cc20a79d471374f3e4b78de31ae6e269f988578e1e602d75134912af60595fe9b906e50ac63ba6d59285b4f1'
 
-$packageArgs = @{
+# Prep 32bit install
+$32bit = $false
+if ($PackageParameters) {
+  if ($PackageParameters["32bit"]) {
+    $32bit = $true
+  }
+}
+
+# Args for 64bit install
+$packageArgs64 = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $urlPackage
+  url           = $url64
   softwareName  = 'wwphone*'
-  checksum      = $checksumPackage
+  checksum      = $checksum64
   checksumType  = 'sha512'
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  silentArgs    = '/MOVEEXISTING /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NOCLOSEAPPLICATIONS /SP-' +
+  " /Log=`"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.Install.log`""
 }
 
-Install-ChocolateyPackage @packageArgs 
+# Args for 32bit install
+$packageArgs32 = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'wwphone*'
+  checksum      = $checksum
+  checksumType  = 'sha512'
+  silentArgs    = '/MOVEEXISTING /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NOCLOSEAPPLICATIONS /SP-' +
+  " /Log=`"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.Install.log`""
+}
+
+if ($32bit) {
+  Write-Host "Installing 32-bit version"
+  Install-ChocolateyPackage @packageArgs32
+} else {
+  Write-Host "Installing 64-bit version"
+  Install-ChocolateyPackage @packageArgs64
+}
