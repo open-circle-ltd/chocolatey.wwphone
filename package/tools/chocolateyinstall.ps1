f6725d6bf5715d050c2cdf4ef494f27eee96f222
kve-occ <210706414+kve-occ@users.noreply.github.com>
2026-05-18 13:09:45 +0200

    fix: 64-bit url

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $PackageParameters = Get-PackageParameters
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://wwcom.ch/downloads/wwphone_4_3_3.exe'
-$url64 = 'https://wwcom.ch/downloads/cti64_4_3_3.exe'
+$url64 = 'https://wwcom.ch/downloads/wwphone64_4_3_3.exe'
 $checksum = '1d6354fdb514bd3cf3b910184a1cf44f83d519cc432d816d6511a86f2d704e5d4860b8766abed070ef64e3454cfcbb5cafccddd49ebc87c97ab6e692d23259e7'
 $checksum64 = '8e97c15235aa3e0aa5ef17c3e1a819d0cd24e085250ccb41fa51fc0e980c60add837842f927097648c0dddba302eeaf110841604cb10da335792a7ece9f94e6b'
 
